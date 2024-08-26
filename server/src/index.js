75357cd9f978a15cddf5351ac94c99e2425bc1de
supunHMSM <152811648+supunHM@users.noreply.github.com>
2024-08-26 15:40:28 +0530

    add middlewares and listen to port

--- server/src/index.js
+++ server/src/index.js
@@ -5,5 +5,12 @@ const axios = require("axios");
 const app = express();
 
 //middle wares
+app.use(express.json());
+app.use(cors());
+
+//listen to port
+app.listen(5000 , ()=>{
+    console.log("SERVER STARTED")
+})
 
 
